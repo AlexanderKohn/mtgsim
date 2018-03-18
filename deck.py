c24e6b5ea5aa9929ae18f6ae0b1caef4e85702de
Alexander Kohn <akohn@mit.edu>
2018-03-18 19:19:49 -0400

    Added card drawing, fleshed out decks

--- deck.py
+++ deck.py
@@ -1,5 +1,41 @@
+import random
+
+
 class Deck:
 	"""Holds information about our deck"""
 
 	def __init__(self):
 		self.cardList = [0] * 10
+
+	def __str__(self):
+		return str(self.cardList)
+	
+	def shuffle(self):
+		self.deck = list(self.cardList)
+	#	numCards = 0
+	#	for i in cardList:
+	#		numCards += i
+	#
+	#	tempList = list(self.cardList)
+	#	
+	#
+	#	self.deck = []
+	#	while 
+
+	def draw(self):
+		numCards = 0
+		for cardCount in self.deck:
+			numCards += cardCount
+
+		cardNum = random.randrange(numCards)
+
+		cardID = 0
+		while cardNum >= 0:
+			cardNum -= self.deck[cardID]
+			cardID += 1
+		cardID -= 1
+		self.deck[cardID] -= 1
+		return cardID
+		
+		
+
--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-
-
+import random
 import deck
-deck1 = deck.Deck()
-deck2 = deck.Deck()
+
+
+def main():
+	deck1 = deck.Deck()
+	deck2 = deck.Deck()
+
+	deck1.cardList = [20,3,5,4,4,3,1,0,0,0]
+	deck2.cardList = deck1.cardList
+
+	print deck1
+	print deck2
+
+	playGame(deck1, deck2, 1)
+
+def playGame(deck1, deck2, verbosity=None):
+
+	if verbosity==None:
+		verbosity = 0
+	
+	deck1.shuffle()
+	deck2.shuffle()
+
+	hand1 = []
+	hand2 = []
+
+	if verbosity > 0:
+		hand1.append(deck1.draw())
+		print "Player 1's hand:" + str(hand1)
+
+		
+	
+	
+
+
+
+
+
+if __name__ == '__main__':
+	main()
